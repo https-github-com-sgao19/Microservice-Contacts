0398e9915c464f1ef37212e4cb23d6ac34ed5657
Zeyu Zhang <zeyuzhang9@gmail.com>
2022-12-07 14:38:04 -0500

    Edit DB config

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -10,9 +10,9 @@ class ColumbiaStudentResource:
     @staticmethod
     def _get_connection():
         conn = pymysql.connect(
-            user=getenv("USER"),
-            password=getenv("PWD"),
-            host=getenv("HOST"),
+            user="admin",
+            password="12345678",
+            host="coms-6156-contacts.cjpbkidulxni.us-east-1.rds.amazonaws.com",
             cursorclass=pymysql.cursors.DictCursor,
             autocommit=True
         )
